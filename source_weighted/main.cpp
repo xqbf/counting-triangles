3e794247e72081a72086ff5c2327ea24aa288bdf
xqbf <57244731+xqbf@users.noreply.github.com>
2024-07-06 12:11:02 +0800

    Add files via upload

--- source_weighted/main.cpp
+++ source_weighted/main.cpp
@@ -1,9 +1,8 @@
 #include "commonfunctions.h"
 #include "temporal_graph.h"
+#include "online.h"
+#include "measure.h"
 #include "tree.h"
-#include "kdtree.h"
-//#include "LSC.h"
-#include "online.h" 
 
 TemporalGraph * build(char * argv[]) {
 
@@ -26,6 +25,7 @@ signed main(int argc,char* argv[]){
 
     if(std::strcmp(argv[4], "Online") == 0){
         std::cout << "Running online search..." << std::endl;
+        //while(1);
         onlineindex* Index=new onlineindex(Graph);
         double start_time = clock();
         online(Index, Graph, argv[2], argv[3]);
@@ -33,65 +33,68 @@ signed main(int argc,char* argv[]){
         delete Graph;
     }
 
-    /*if(std::strcmp(argv[argc-1], "Tree") == 0){
-        std::cout << "Running tree index..." << std::endl;
-        treeindex* Index=new treeindex(Graph);
-        double start_time = clock();
-        if(argc>5){
-            for(int i=2;i<=5;i++){
-                tree(Index,Graph,argv[i],argv[i+4]);
-                std::cout << "Tree index solution completed in " << timeFormatting((clock()-start_time)/CLOCKS_PER_SEC).str() << std::endl;
-            }
-
-        }
-        else{
-        tree(Index, Graph, argv[2], argv[3]);
-        std::cout << "Tree index solution completed in " << timeFormatting((clock()-start_time)/CLOCKS_PER_SEC).str() << std::endl;
-        }
-        //while(1);
-        //delete Graph;
-    }
     if(std::strcmp(argv[4], "Measure") == 0){
         std::cout << "Running measure..." << std::endl;
         measureindex* Index=new measureindex(Graph);
         double start_time = clock();
         std::cout << "Array index solution completed in " << timeFormatting((clock()-start_time)/CLOCKS_PER_SEC).str() << std::endl;
         delete Graph;
-    }*/
+    }
 
-    if(std::strcmp(argv[argc-1], "KD") == 0){
-        std::cout << "Running kd tree index..." << std::endl;
-        kdindex* Index=new kdindex(Graph,0.8);
-        std::cout<<"Updating kd tree index..." <<std::endl;
-        Index->update(Graph,0.8);
+    /*if(std::strcmp(argv[argc-1], "LSC") == 0){
+        std::cout << "Running LSC index..." << std::endl;
+        LSCindex* Index=new LSCindex(Graph);
         double start_time = clock();
-
-        kd(Index, Graph, argv[2], argv[3]);
-        std::cout << "kd tree index solution completed in " << timeFormatting((clock()-start_time)/CLOCKS_PER_SEC).str() << std::endl;
-        std::cerr << "Over!\n";
+        if(argc>5){
+            for(int i=2;i<=10;i++){
+                LSC(Index,Graph,argv[i],argv[i+9]);
+                std::cout << "LSC index solution completed in " << timeFormatting((clock()-start_time)/CLOCKS_PER_SEC).str() << std::endl;
+            }
+        }
+        else{
+        LSC(Index, Graph, argv[2], argv[3]);
+        std::cout << "LSC index solution completed in " << timeFormatting((clock()-start_time)/CLOCKS_PER_SEC).str() << std::endl;
+        }
+        std::cerr<<"Over!\n";
         while(1);
         //delete Graph;
-    }
-    
+    }*/
+
     if(std::strcmp(argv[argc-1], "Tree") == 0){
-        std::cout << "Running tree index..." << std::endl;
-        treeindex* Index=new treeindex(Graph,0.8);
-        std::cout<<"Updating tree index..." <<std::endl;
-        Index->update(Graph,0.8);
+        std::cout << "Running Wavelet tree index..." << std::endl;
+        treeindex* Index=new treeindex(Graph);
         double start_time = clock();
         if(argc>5){
             for(int i=2;i<=10;i++){
                 tree(Index,Graph,argv[i],argv[i+9]);
-                std::cout << "Tree index solution completed in " << timeFormatting((clock()-start_time)/CLOCKS_PER_SEC).str() << std::endl;
+                std::cout << "Wavelet tree index solution completed in " << timeFormatting((clock()-start_time)/CLOCKS_PER_SEC).str() << std::endl;
             }
-            std::cerr << "Over!\n";
         }
         else{
         tree(Index, Graph, argv[2], argv[3]);
-        std::cout << "tree index solution completed in " << timeFormatting((clock()-start_time)/CLOCKS_PER_SEC).str() << std::endl;
-        std::cerr << "Over!\n";
+        std::cout << "Wavelet tree index solution completed in " << timeFormatting((clock()-start_time)/CLOCKS_PER_SEC).str() << std::endl;
         }
-        while(1);
+        std::cerr<<"Over!\n";
+        //while(1);
         //delete Graph;
     }
+
+    /*if(std::strcmp(argv[argc-1], "KD") == 0){
+        std::cout << "Running KD index..." << std::endl;
+        kdindex* Index=new kdindex(Graph);
+        double start_time = clock();
+        if(argc>5){
+            for(int i=2;i<=10;i++){
+                kd(Index,Graph,argv[i],argv[i+9]);
+                std::cout << "KD index solution completed in " << timeFormatting((clock()-start_time)/CLOCKS_PER_SEC).str() << std::endl;
+            }
+        }
+        else{
+        kd(Index, Graph, argv[2], argv[3]);
+        std::cout << "KD index solution completed in " << timeFormatting((clock()-start_time)/CLOCKS_PER_SEC).str() << std::endl;
+        }
+        std::cerr<<"Over!\n";
+        while(1);
+        //delete Graph;
+    }*/
 }
